e19a3b3bc68ee8638e554b2ce628b731902a4020
TaiDoan <doananhtai20@gmail.com>
2025-05-16 11:31:46 +0700

    updatetudau

---
+++ my-portfolio/Tai-Portfolio/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './globals.css'
+
+const page = () => {
+  return (
+    <div className='nav__link'>page</div>
+  )
+}
+
+export default page
---
+++ my-portfolio/Tai-Portfolio/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}", // Quét các tệp trong thư mục pages
+    "./components/**/*.{js,ts,jsx,tsx}", // Quét các tệp trong thư mục components
+    "./app/**/*.{js,ts,jsx,tsx}", // Quét các tệp trong thư mục app nếu bạn sử dụng thư mục app
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
